db0d59c9bfdd9e874f9cb14b76aa0072b9a5c20b
susmitha <pillisusmitha097@gmail.com>
2023-10-24 21:31:06 +0530

    add template files

--- store/views.py
+++ store/views.py
@@ -7,4 +7,5 @@ def home (request):
     products = Product.objects.all()
     return render(request, 'home.html',{'products' : products})
 
-
+def about(request):
+    return render(request, 'about.html', {})
